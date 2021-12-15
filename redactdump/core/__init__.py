9ddcdd2bcdefc36db8c3442eb390d5fb1c39755a
Mathias V. Nielsen <1547127+math280h@users.noreply.github.com>
2021-12-15 08:04:59 +0100

    Linting

--- redactdump/core/__init__.py
+++ redactdump/core/__init__.py
@@ -1,4 +1,4 @@
-from redactdump.core.redactor import Redactor
 from redactdump.core.config import Config
-from redactdump.core.file import File
 from redactdump.core.database import Database
+from redactdump.core.file import File
+from redactdump.core.redactor import Redactor
